9426644870a891d688442df84dea334c4b2be019
Alex Leferry 2 <alexherbo2@gmail.com>
2019-11-06 11:50:47 +0100

    Website: Add buttons for commands

--- website/script.js
+++ website/script.js
@@ -1,4 +1,18 @@
+const parseButtonKeys = (button) => {
+  const keyChord = Array.from(button.querySelectorAll('kbd'), (kbd) => [
+    kbd.hasAttribute('shift') && 'Shift',
+    kbd.getAttribute('code') || kbd.textContent,
+  ].filter(Boolean)).flat()
+  return keyChord
+}
+
 const DOMContentLoaded = (event) => {
+  const modes = { modal, pass }
+  for (const button of document.querySelectorAll('button.krabby')) {
+    const mode = modes[button.getAttribute('mode')]
+    const keyChord = parseButtonKeys(button)
+    button.addEventListener('click', (event) => mode.play(keyChord))
+  }
   for (const carousel of document.querySelectorAll('.carousel')) {
     new Carousel(carousel)
   }
